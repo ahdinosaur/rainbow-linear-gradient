e354a199cb237bb1afac29b8402dc9614b9a6a79
Michael Williams <michael.williams@enspiral.com>
2016-08-27 17:19:02 +1200

    allow passing in instantiated ndarray
    so can work with ndarray-scratch for more perf / less gc

--- index.js
+++ index.js
@@ -1,40 +1,52 @@
 var defined = require('defined')
 var mod = require('mod-op')
-var ndarray = require('ndarray')
+var Ndarray = require('ndarray')
 
 module.exports = rainbowGradient
 
 function rainbowGradient (opts) {
   opts = defined(opts, {})
+
   var start = defined(opts.start, 0)
   var step = defined(opts.step, 1)
   var length = defined(opts.length, 10)
+
   var step = opts.step
   if (typeof step === 'function') {
     step = step(length)
   } else if (step == null) {
     step = 1 / length
   }
+
   var saturation = defined(opts.saturation, 100)
   var lightness = defined(opts.lightness, 50)
 
-  var colors = new Float32Array(length * 3)
-  for (var c = 0, i = 0; c < colors.length; c++, i += 3) {
-    colors[i] = mod(
+  var ndarray = opts.ndarray || Ndarray(
+    new Float32Array(length * 3),
+    [length, 3]
+  )
+
+  return createRainbowGradient(start, step, length, saturation, lightness, ndarray)
+}
+
+function createRainbowGradient (start, step, length, saturation, lightness, ndarray) {
+  var data = ndarray.data
+
+  for (var c = 0, i = 0; c < data.length; c++, i += 3) {
+    data[i] = mod(
+      (
         (
           (c * step)
           * 360
         )
         + start
-      , 360
+      ), 360
     )
-    colors[i + 1] = saturation
-    colors[i + 2] = lightness
+    data[i + 1] = saturation
+    data[i + 2] = lightness
   }
 
-  var ret = ndarray(colors, [length, 3])
+  ndarray.format = 'hsl'
 
-  ret.format = 'hsl'
-
-  return ret
+  return ndarray
 }
